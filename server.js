03f9bdee8e240e769339355e6aa5d6483b77884d
Louis Calderone <loucalderone@Louis-Calderones-MacBook.local>
2012-01-01 00:48:40 -0500

    happy new year! :)

--- server.js
+++ server.js
@@ -8,63 +8,77 @@ var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NO
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
 console.log("Connecting to " + host + ":" + port);
-var db = new Db('songs', new Server(host, port, {}), {native_parser:false});
+var mongo = new Db('songs', new Server(host, port, {}), {native_parser:false});
+mongo.open(function(err, db) {
+	//
+	// Create a Router
+	//
+	var router = new(journey.Router);
 
-//
-// Create a Router
-//
-var router = new(journey.Router);
-
-// Create the routing table
-router.map(function () {
-    this.root.bind(function (req, res) { res.send('This should be accessed by authorized applications only.') });
-    this.get(/^song\/([0-9]+)$/).bind(function (req, res, id, params) {
-        db.open(function(err, db) {
-            res.send(200, {}, {});
-        });
-    });
-    this.get('/ac').bind(function (req, resp, data) {
-		console.log(data.term);
-		var iTunesOptions = {
-		  host: 'itunes.apple.com',
-		  port: 80,
-		  path: '/search?term=' + data.term + '&entity=musicArtist&attribute=artistTerm&limit=5'
-		};
-		var jsonString = "";
-		console.log(iTunesOptions.path)
-		var req = http.request(iTunesOptions, function(res) {
-		  console.log('STATUS: ' + res.statusCode);
-		  console.log('HEADERS: ' + JSON.stringify(res.headers));
-		  res.setEncoding('utf8');
-		  res.on('data', function (chunk) {
-			console.log('CHUNK:' + chunk)
-		    jsonString += chunk;
-		  });
-		  res.on('end', function(){
-			var itunesJson = JSON.parse(jsonString)
-   			db.open(function(err, db) {
-				db.collection('itunes', function(err, collection) {
-				  for(var x in itunesJson.results) {
-				    collection.insert(itunesJson.results[x]);
-				  }
+	// Create the routing table
+	router.map(function () {
+	    this.root.bind(function (req, res) { res.send('This should be accessed by authorized applications only.') });
+	    this.get(/^song\/([0-9]+)$/).bind(function (req, res, id, params) {
+	        db.open(function(err, db) {
+	            res.send(200, {}, {});
+	        });
+	    });
+	    this.get('/ac').bind(function (req, resp, data) {
+		  if(data.term){
+			console.log(data.term);
+			var iTunesOptions = {
+			  host: 'itunes.apple.com',
+			  port: 80,
+			  path: '/search?term=' + data.term + '&entity=musicArtist&attribute=artistTerm&limit=5'
+			};
+			var jsonResults;
+			db.collection('itunes', function(err, collection) {
+				collection.find({artistName:new RegExp(data.term,"i")}, function(err, cursor) {
+					cursor.toArray(function(err,docs) {
+						if(docs.length > 0) {
+							resp.send(200,{},docs)
+						} else {
+							var jsonString = "";
+							console.log(iTunesOptions.path)
+							var req = http.request(iTunesOptions, function(res) {
+							  console.log('Made Request - STATUS: ' + res.statusCode);
+							  //console.log('HEADERS: ' + JSON.stringify(res.headers));
+							  res.setEncoding('utf8');
+							  res.on('data', function (chunk) {
+								//console.log('CHUNK:' + chunk)
+							    jsonString += chunk;
+							  });
+							  res.on('end', function(){
+								var itunesJson = JSON.parse(jsonString)
+								db.collection('itunes', function(err, collection) {
+								  for(var x in itunesJson.results) {
+								    collection.insert(itunesJson.results[x]);
+								  }
+								});
+								resp.send(200, {}, itunesJson);
+							  });
+							});
+				  	        req.end();
+					    }
+					});
 				});
-			});
-			resp.send(200, {}, itunesJson);
 		  });
-		});
-		req.end();        
-    });
+		  } else {
+			resp.send("need a term");
+		  }        
+	    });
+	});
+
+	require('http').createServer(function (request, response) {
+	    var body = "";
+
+	    request.addListener('data', function (chunk) { body += chunk });
+	    request.addListener('end', function () {
+
+	        router.handle(request, body, function (result) {
+	            response.writeHead(result.status, result.headers);
+	            response.end(result.body);
+	        });
+	    });
+	}).listen(8080);
 });
-
-require('http').createServer(function (request, response) {
-    var body = "";
-
-    request.addListener('data', function (chunk) { body += chunk });
-    request.addListener('end', function () {
-
-        router.handle(request, body, function (result) {
-            response.writeHead(result.status, result.headers);
-            response.end(result.body);
-        });
-    });
-}).listen(8080);
