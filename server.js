1d43baf8144fcbc80061aa8e8c9c3e60861c7f6b
loucal <louis.calderone@gmail.com>
2012-01-08 10:15:17 -0500

    tabs to spaces

--- server.js
+++ server.js
@@ -12,49 +12,49 @@ var mongo = new Db('songs', new Server(host, port, {}), {native_parser:false});
 mongo.open(function(err, db) {
   
   function call_itunes_and_respond(term, type, response) {
-		var iTunesOptions = { host: 'itunes.apple.com', port: 80 };
-	  if (type === "a") {
-			iTunesOptions.path = '/search?term=' + term.replace(" ", "+", "g") + '&entity=musicArtist&attribute=artistTerm&limit=5'
-			var c = 'artists'
-			var matchJson = {_id:term.toLowerCase()}
-		} else if (type === "s") {
-			//do song path
-			var c = 'songs'
-			var matchJson = {_id:term}
-		}
-		var jsonResults;
-	  db.collection(c, function(err, collection) {
-	    collection.find(matchJson, function(err, cursor) {
-	      cursor.toArray(function(err,docs) {
-	        if(docs.length > 0) {
-	          response.send(200,{},docs)
-	        } else {
-	          var jsonString = "";
-	          console.log(iTunesOptions.path)
-	          var req = http.request(iTunesOptions, function(res) {
-	            console.log('Made Request - STATUS: ' + res.statusCode);
-	            res.setEncoding('utf8');
-	            res.on('data', function (chunk) {
-	              jsonString += chunk;
-	            });
-	            res.on('end', function(){
-	              var itunesJson = JSON.parse(jsonString)
-	              db.collection(c, function(err, collection) {
-	                for(var x in itunesJson.results) {					        	
-										itunesJson.results[x]._id = itunesJson.results[x].artistName.toLowerCase()
-	                  collection.insert(itunesJson.results[x]);
-	                }
-	              });
-	              response.send(200, {}, itunesJson.results);
-	            });
-	          });
-	          req.end();
-	        }
-	      });
-	    });
-	  });
-	}
-	
+    var iTunesOptions = { host: 'itunes.apple.com', port: 80 };
+    if (type === "a") {
+      iTunesOptions.path = '/search?term=' + term.replace(" ", "+", "g") + '&entity=musicArtist&attribute=artistTerm&limit=5'
+      var c = 'artists'
+      var matchJson = {_id:term.toLowerCase()}
+    } else if (type === "s") {
+      //do song path
+      var c = 'songs'
+      var matchJson = {_id:term}
+    }
+    var jsonResults;
+    db.collection(c, function(err, collection) {
+      collection.find(matchJson, function(err, cursor) {
+        cursor.toArray(function(err,docs) {
+          if(docs.length > 0) {
+            response.send(200,{},docs)
+          } else {
+            var jsonString = "";
+            console.log(iTunesOptions.path)
+            var req = http.request(iTunesOptions, function(res) {
+              console.log('Made Request - STATUS: ' + res.statusCode);
+              res.setEncoding('utf8');
+              res.on('data', function (chunk) {
+                jsonString += chunk;
+              });
+              res.on('end', function(){
+                var itunesJson = JSON.parse(jsonString)
+                db.collection(c, function(err, collection) {
+                  for(var x in itunesJson.results) {                    
+                    itunesJson.results[x]._id = itunesJson.results[x].artistName.toLowerCase()
+                    collection.insert(itunesJson.results[x]);
+                  }
+                });
+                response.send(200, {}, itunesJson.results);
+              });
+            });
+            req.end();
+          }
+        });
+      });
+    });
+  }
+  
   var router = new(journey.Router);
 
   // Create the routing table
@@ -62,7 +62,7 @@ mongo.open(function(err, db) {
     this.root.bind(function (req, res) { res.send('This should be accessed by authorized applications only.') });
     this.get(/^song\/([0-9]+)$/).bind(function (req, res, id, params) {
       db.collection('songs', function(err, collection) {
-				collection.find()
+        collection.find()
         res.send(200, {}, {});
       });
     });
